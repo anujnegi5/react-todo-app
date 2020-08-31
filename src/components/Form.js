ea14bbb1c900870520c7fa49ed2807d1a7fd6c8d
Anuj Negi <anuj5negi5@gmail.com>
2020-08-31 22:24:22 +0530

    finished project"

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Form = ({ inputText, setInputText, todos, setTodos }) => {
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
   const inputTextHandler = (e) => {
     setInputText(e.target.value);
   };
@@ -9,10 +9,15 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
     e.preventDefault();
     setTodos([
       ...todos,
-      { text: inputText, completed: false, id: Math.random * 1000 },
+      { text: inputText, completed: false, id: Math.random() * 1000 },
     ]);
     setInputText("");
   };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+
   return (
     <form>
       <input
@@ -25,7 +30,7 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            filteredTodos={filteredTodos}
+            todos={todos}
+            todo={todo}
+            setTodos={setTodos}
+            key={todo.id}
+            text={todo.text}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
